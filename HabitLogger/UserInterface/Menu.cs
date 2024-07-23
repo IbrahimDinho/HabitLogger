ac0d8c5e3498e5291ca6784ec51de62140579ba8
IbrahimDinho <ibbyi7i@yahoo.com>
2024-07-23 02:40:29 +0100

    Menu and user input added

---
+++ HabitLogger/UserInterface/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitLogger.UserInterface
+{
+    public class Menu
+    {
+        public int NumberOfOptions { get; } = Enum.GetValues(typeof(MenuOptions)).Length - 1;
+
+        private const string Name = "Main Menu";
+        private readonly string Question = "What would you like to do from options below?";
+        private readonly string dashedLine = "---------------------------------";
+
+        public enum MenuOptions
+        {
+
+            Close = 0,
+            View = 1,
+            Insert = 2,
+            Delete = 3,
+            Update = 4
+        }
+
+        public void ShowMenu()
+        {
+            Console.WriteLine( Name + Environment.NewLine);
+            
+            Console.WriteLine(Question);
+
+            Console.WriteLine($"Type {(int)MenuOptions.Close} to {MenuOptions.Close} application");
+            Console.WriteLine($"Type {(int)MenuOptions.View} to {MenuOptions.View} all records");
+            Console.WriteLine($"Type {(int)MenuOptions.Insert} to {MenuOptions.Insert} Record");
+            Console.WriteLine($"Type {(int)MenuOptions.Delete} to {MenuOptions.Delete} Record");
+            Console.WriteLine($"Type {(int)MenuOptions.Update} to {MenuOptions.Update} Record");
+
+            Console.WriteLine(dashedLine);
+        }
+    }
+}
---
+++ HabitLogger/UserInterface/UserInput.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HabitLogger.Menu;
+
+namespace HabitLogger.UserInterface
+{
+    public class UserInput
+    {
+        private int validInput;
+        private int numberOfUserOptions;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="numberOfOptions"></param>
+        public UserInput(int numberOfOptions) 
+        { 
+            this.numberOfUserOptions = numberOfOptions;
+        }
+
+        
+        /// <summary>
+        /// Get user input
+        /// </summary>
+        /// <returns>Integer between 0 - 4 (current length of enum)</returns>
+        public int GetValidatedUserInput()
+        {
+
+            string input = Console.ReadLine();
+            bool isValid = false;
+
+            while (isValid == false)
+            {
+                isValid = ValidateInput(input);
+            }
+
+            return validInput;
+        }
+
+        /// <summary>
+        /// Validate input. Must be an integer
+        /// </summary>
+        /// <param name="userInput"></param>
+        /// <returns></returns>
+        private bool ValidateInput(string userInput)
+        {
+            if (userInput == null)
+            {
+                Console.WriteLine($"Input value {userInput} is Invalid");
+                return false;
+            }
+            int parsedInteger;
+            bool validInteger = int.TryParse(userInput, out parsedInteger);
+            if (parsedInteger < 0 || parsedInteger > numberOfUserOptions)
+            {
+                Console.WriteLine($"Input value {userInput} is Invalid");
+                return false;
+            }
+            validInput = parsedInteger;
+            
+            return true;
+
+
+        }
+    }
+}
