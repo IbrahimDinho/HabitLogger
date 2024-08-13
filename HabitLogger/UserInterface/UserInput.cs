1704027bb4e5194dffd69db3c8af9ff590569c94
IbrahimDinho <ibbyi7i@yahoo.com>
2024-08-13 02:37:16 +0100

    Deal with user input

--- HabitLogger/UserInterface/UserInput.cs
+++ HabitLogger/UserInterface/UserInput.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static HabitLogger.Menu;
 
 namespace HabitLogger.UserInterface
 {
@@ -21,6 +20,24 @@ namespace HabitLogger.UserInterface
             this.numberOfUserOptions = numberOfOptions;
         }
 
+
+        public string GetDateInput()
+        {
+            Console.WriteLine("Please insert the date of the habit: {Format dd-mm-yy}.");
+
+            string dateInput = Console.ReadLine();
+            //ValidateInput(dateInput);
+            return dateInput;
+        }
+
+        public int GetHoursInput()
+        {
+            Console.WriteLine("Please insert the number hours of the habit.");
+
+            string hoursInput = Console.ReadLine();
+            //validateHoours
+            return int.Parse(hoursInput);
+        }
         
         /// <summary>
         /// Get user input
